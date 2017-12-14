a6b79b03ef1abd5375ab3846a343c55325b5b0ec
Cristina Gatt <gattcristina112@gmail.com>
2017-12-14 16:44:16 +0000

    Test :)

---
+++ cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_5c3e5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_5c3e5.dir/feature_tests.c.obj"
+  "cmTC_5c3e5.pdb"
+  "cmTC_5c3e5.exe"
+  "cmTC_5c3e5.exe.manifest"
+  "libcmTC_5c3e5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_5c3e5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_5c3e5.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_5c3e5.
+# This may be replaced when dependencies are built.
