d5d460f36423fa15ade9cfb324ed750fe030aedc
Larasultana <32870192+Larasultana@users.noreply.github.com>
2017-12-14 12:30:02 +0100

    start of calculator

---
+++ CPS1010Calculator/cmake-build-debug/CMakeFiles/CPS1010Calculator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPS1010Calculator.dir/main.c.o"
+  "CPS1010Calculator.pdb"
+  "CPS1010Calculator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CPS1010Calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
